18b27456fa2d0ac5d66a3f35ebd98e169199963c
gunerkaanalkim <g.kaanalkim@gmail.com>
2024-04-02 21:23:28 +0100

    github workflow

--- src/main/java/org/kaanalkim/authserver/service/impl/RoleUserServiceImpl.java
+++ src/main/java/org/kaanalkim/authserver/service/impl/RoleUserServiceImpl.java
@@ -1,7 +1,8 @@
 package org.kaanalkim.authserver.service.impl;
 
-import org.kaanalkim.authserver.exception.NotFoundException;
+import lombok.AllArgsConstructor;
 import org.kaanalkim.authserver.exception.RoleAlreadyAssignedException;
+import org.kaanalkim.authserver.model.Permission;
 import org.kaanalkim.authserver.model.Role;
 import org.kaanalkim.authserver.model.RoleUser;
 import org.kaanalkim.authserver.model.User;
@@ -10,17 +11,23 @@ import org.kaanalkim.authserver.payload.dto.RoleDTO;
 import org.kaanalkim.authserver.payload.dto.RoleUserDTO;
 import org.kaanalkim.authserver.payload.dto.UserDTO;
 import org.kaanalkim.authserver.payload.request.RoleToUser;
+import org.kaanalkim.authserver.payload.response.AuthorizationVerificationResponse;
 import org.kaanalkim.authserver.repository.RoleUserRepository;
+import org.kaanalkim.authserver.service.RolePermissionService;
 import org.kaanalkim.authserver.service.RoleService;
 import org.kaanalkim.authserver.service.RoleUserService;
 import org.kaanalkim.authserver.service.UserService;
 import org.kaanalkim.authserver.service.base.AbstractCrudService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor
 public class RoleUserServiceImpl extends AbstractCrudService<RoleUser, RoleUserDTO> implements RoleUserService {
     private final RoleUserRepository roleUserRepository;
     private final RoleUserMapper roleUserMapper;
@@ -29,16 +36,7 @@ public class RoleUserServiceImpl extends AbstractCrudService<RoleUser, RoleUserD
     private final RoleService roleService;
     private final UserService userService;
 
-    @Autowired
-    public RoleUserServiceImpl(RoleUserRepository roleUserRepository, RoleUserMapper roleUserMapper, RoleMapper roleMapper, UserMapper userMapper1, RoleService roleService, UserService userService) {
-        this.roleUserRepository = roleUserRepository;
-        this.roleUserMapper = roleUserMapper;
-        this.roleMapper = roleMapper;
-        this.userMapper = userMapper1;
-        this.roleService = roleService;
-        this.userService = userService;
-    }
-
+    private final RolePermissionService rolePermissionService;
 
     @Override
     @SuppressWarnings("unchecked")
@@ -53,11 +51,6 @@ public class RoleUserServiceImpl extends AbstractCrudService<RoleUser, RoleUserD
     }
 
     @Override
-    public Optional<RoleUser> findByRoleAndUser(Role role, User user) {
-        return this.roleUserRepository.findByRoleAndUser(role, user);
-    }
-
-    @Override
     public RoleUser assignRoleToUser(RoleToUser roleToUser) {
         RoleDTO roleDTO = this.roleService.get(roleToUser.getRoleId());
         UserDTO userDTO = this.userService.get(roleToUser.getUserId());
@@ -65,7 +58,7 @@ public class RoleUserServiceImpl extends AbstractCrudService<RoleUser, RoleUserD
         Role role = this.roleMapper.toEntity(roleDTO);
         User user = this.userMapper.toEntity(userDTO);
 
-        Optional<RoleUser> oldRoleAndUser = this.findByRoleAndUser(role, user);
+        Optional<RoleUser> oldRoleAndUser = getRepository().findByUser(user);
 
         oldRoleAndUser.ifPresent(roleUser -> {
             throw new RoleAlreadyAssignedException(super.getErrorMessage(ErrorCode.ROLE_ALREADY_ASSIGNED, roleUser.getId().toString()));
@@ -83,20 +76,62 @@ public class RoleUserServiceImpl extends AbstractCrudService<RoleUser, RoleUserD
 
     @Override
     public RoleUser revokeRoleToUser(RoleToUser roleToUser) {
-        RoleDTO roleDTO = this.roleService.get(roleToUser.getRoleId());
         UserDTO userDTO = this.userService.get(roleToUser.getUserId());
 
-        Role role = this.roleMapper.toEntity(roleDTO);
         User user = this.userMapper.toEntity(userDTO);
 
-        Optional<RoleUser> oldRoleAndUser = this.findByRoleAndUser(role, user);
+        Optional<RoleUser> oldRoleAndUser = getRepository().findByUser(user);
 
-        if(!oldRoleAndUser.isPresent()) {
-            throw new NotFoundException(super.getErrorMessage(ErrorCode.ROLE_USER_NOT_FOUND, ""));
+        if (oldRoleAndUser.isEmpty()) {
+            return null;
         }
 
         this.roleUserRepository.delete(oldRoleAndUser.get());
 
         return oldRoleAndUser.get();
+    }
+
+    @Override
+    public RoleUser findByUserId(int userId) {
+        UserDTO userDTO = this.userService.get((long) userId);
+
+        User user = this.userMapper.toEntity(userDTO);
+
+        Optional<RoleUser> roleUser = getRepository().findByUser(user);
+
+        return roleUser.orElse(null);
+    }
+
+    @Override
+    public AuthorizationVerificationResponse hasPermission(int userId, String requestPath) {
+        UserDTO userDTO = this.userService.get((long) userId);
+        User user = this.userMapper.toEntity(userDTO);
+
+        Optional<RoleUser> roleUser = this.roleUserRepository.findByUser(user);
+
+        if (roleUser.isPresent()) {
+            List<Permission> assignedPermissionsOfRole = this.rolePermissionService
+                    .getAssignedPermissionsOfRole(roleUser.get().getRole().getId());
+
+            Permission assignedPermission = assignedPermissionsOfRole
+                    .parallelStream()
+                    .filter(permission -> {
+                        String requestPathOfPermission = String.format("/%s/%s", permission.getController(), permission.getUrl());
+
+                        return requestPathOfPermission.equalsIgnoreCase(requestPath);
+                    })
+                    .findFirst()
+                    .orElse(null);
+
+            if (Objects.nonNull(assignedPermission)) {
+                return AuthorizationVerificationResponse.builder()
+                        .hasPermission(true)
+                        .build();
+            }
+        }
+
+        return AuthorizationVerificationResponse.builder()
+                .hasPermission(false)
+                .build();
     }
 }
