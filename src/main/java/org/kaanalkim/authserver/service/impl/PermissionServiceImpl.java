86395f4d18241d5d73a6262d031a6ab345a4e7b6
gunerkaanalkim <g.kaanalkim@gmail.com>
2024-04-12 14:52:45 +0100

    #7

--- src/main/java/org/kaanalkim/authserver/service/impl/PermissionServiceImpl.java
+++ src/main/java/org/kaanalkim/authserver/service/impl/PermissionServiceImpl.java
@@ -1,44 +1,20 @@
 package org.kaanalkim.authserver.service.impl;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.kaanalkim.authserver.mapper.PermissionMapper;
+import lombok.AllArgsConstructor;
 import org.kaanalkim.authserver.model.Permission;
-import org.kaanalkim.authserver.payload.dto.HasPermissionDTO;
-import org.kaanalkim.authserver.payload.dto.PermissionDTO;
 import org.kaanalkim.authserver.repository.PermissionRepository;
 import org.kaanalkim.authserver.service.PermissionService;
 import org.kaanalkim.authserver.service.base.AbstractCrudService;
 import org.springframework.stereotype.Service;
 
-import lombok.AllArgsConstructor;
-
 @Service
 @AllArgsConstructor
-public class PermissionServiceImpl extends AbstractCrudService<Permission, PermissionDTO> implements PermissionService {
+public class PermissionServiceImpl extends AbstractCrudService<Permission> implements PermissionService {
     private final PermissionRepository permissionRepository;
-    private final PermissionMapper permissionMapper;
-
-    @Override
-    public Optional<Permission> findByControllerAndPermissionName(String controller, String permissionName) {
-        return this.permissionRepository.findByControllerAndTitle(controller, permissionName);
-    }
-
-    @Override
-    public List<HasPermissionDTO> getAllWithHasPermission(Long roleId) {
-        return this.permissionRepository.getAllWithHasPermission(roleId);
-    }
 
     @Override
     @SuppressWarnings("unchecked")
     protected PermissionRepository getRepository() {
         return this.permissionRepository;
-    }
-
-    @Override
-    @SuppressWarnings("unchecked")
-    protected PermissionMapper getMapper() {
-        return this.permissionMapper;
     }
 }
