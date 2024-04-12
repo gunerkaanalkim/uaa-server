80b3493abf6cef17ed3b2e0bb8d453586605ab70
gunerkaanalkim <g.kaanalkim@gmail.com>
2024-04-12 14:52:48 +0100

    #7

--- src/main/java/org/kaanalkim/authserver/controller/RealmController.java
+++ src/main/java/org/kaanalkim/authserver/controller/RealmController.java
@@ -1,24 +1,31 @@
 package org.kaanalkim.authserver.controller;
 
 
+import lombok.AllArgsConstructor;
 import org.kaanalkim.authserver.controller.base.AbstractController;
+import org.kaanalkim.authserver.mapper.RealmMapper;
 import org.kaanalkim.authserver.model.Realm;
 import org.kaanalkim.authserver.payload.dto.RealmDTO;
 import org.kaanalkim.authserver.service.RealmService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.AllArgsConstructor;
-
 @RestController
 @RequestMapping("realm")
 @AllArgsConstructor
 public class RealmController extends AbstractController<Realm, RealmDTO> {
     private final RealmService realmService;
+    private final RealmMapper realmMapper;
 
     @Override
     @SuppressWarnings("unchecked")
     protected RealmService getService() {
         return this.realmService;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    protected RealmMapper getMapper() {
+        return this.realmMapper;
     }
 }
