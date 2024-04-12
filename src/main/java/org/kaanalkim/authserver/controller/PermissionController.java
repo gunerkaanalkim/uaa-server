1f9fbfae88f62310bc6027b8324482c5665b7465
gunerkaanalkim <g.kaanalkim@gmail.com>
2024-04-12 14:52:42 +0100

    #7

--- src/main/java/org/kaanalkim/authserver/controller/PermissionController.java
+++ src/main/java/org/kaanalkim/authserver/controller/PermissionController.java
@@ -1,7 +1,9 @@
 package org.kaanalkim.authserver.controller;
 
 
+import lombok.AllArgsConstructor;
 import org.kaanalkim.authserver.controller.base.AbstractController;
+import org.kaanalkim.authserver.mapper.PermissionMapper;
 import org.kaanalkim.authserver.model.Permission;
 import org.kaanalkim.authserver.payload.dto.PermissionDTO;
 import org.kaanalkim.authserver.service.PermissionService;
@@ -10,16 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("permission")
+@AllArgsConstructor
 public class PermissionController extends AbstractController<Permission, PermissionDTO> {
     private final PermissionService permissionService;
-
-    public PermissionController(PermissionService permissionService) {
-        this.permissionService = permissionService;
-    }
+    private final PermissionMapper permissionMapper;
 
     @Override
     @SuppressWarnings("unchecked")
     protected PermissionService getService() {
         return this.permissionService;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    protected PermissionMapper getMapper() {
+        return this.permissionMapper;
     }
 }
