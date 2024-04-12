a939d1b9b872da21afc99e3423a40051a741dd55
gunerkaanalkim <g.kaanalkim@gmail.com>
2024-04-12 14:52:39 +0100

    #7

--- src/main/java/org/kaanalkim/authserver/mapper/base/BaseMapper.java
+++ src/main/java/org/kaanalkim/authserver/mapper/base/BaseMapper.java
@@ -1,7 +1,7 @@
 package org.kaanalkim.authserver.mapper.base;
 
-import org.kaanalkim.authserver.mapper.AbstractDTO;
 import org.kaanalkim.authserver.model.base.AbstractEntity;
+import org.kaanalkim.authserver.payload.mapper.AbstractDTO;
 
 public interface BaseMapper<T extends AbstractEntity, D extends AbstractDTO> {
     D toDTO(T e);
