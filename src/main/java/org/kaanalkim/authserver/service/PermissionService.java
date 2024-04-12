a6e204a24ca9d13649dda696e1cf38337b4fbf86
gunerkaanalkim <g.kaanalkim@gmail.com>
2024-04-12 14:52:44 +0100

    #7

--- src/main/java/org/kaanalkim/authserver/service/PermissionService.java
+++ src/main/java/org/kaanalkim/authserver/service/PermissionService.java
@@ -1,15 +1,7 @@
 package org.kaanalkim.authserver.service;
 
 import org.kaanalkim.authserver.model.Permission;
-import org.kaanalkim.authserver.payload.dto.HasPermissionDTO;
-import org.kaanalkim.authserver.payload.dto.PermissionDTO;
 import org.kaanalkim.authserver.service.base.BaseCrudService;
 
-import java.util.List;
-import java.util.Optional;
-
-public interface PermissionService extends BaseCrudService<Permission, PermissionDTO> {
-    Optional<Permission> findByControllerAndPermissionName(String controller, String permissionName);
-
-    List<HasPermissionDTO> getAllWithHasPermission(Long roleId);
+public interface PermissionService extends BaseCrudService<Permission> {
 }
