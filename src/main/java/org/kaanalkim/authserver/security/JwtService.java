085fd9f9555f533d38f1a2377b5306ca745bba75
gunerkaanalkim <g.kaanalkim@gmail.com>
2024-04-18 16:22:32 +0100

    #16

--- src/main/java/org/kaanalkim/authserver/security/JwtService.java
+++ src/main/java/org/kaanalkim/authserver/security/JwtService.java
@@ -2,15 +2,16 @@ package org.kaanalkim.authserver.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.kaanalkim.authserver.model.User;
+import org.kaanalkim.authserver.payload.response.JWTVerificationResponse;
+import org.kaanalkim.common.exception.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
-import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -33,22 +34,22 @@ public class JwtService {
         return claimsResolver.apply(claims);
     }
 
-    public String generateToken(UserDetails userDetails) {
-        return generateToken(new HashMap<>(), userDetails);
+    public String generateToken(User user) {
+        return generateToken(new HashMap<>(), user);
     }
 
-    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
-        return doGenerateToken(extraClaims, userDetails);
+    public String generateToken(Map<String, Object> extraClaims, User user) {
+        return doGenerateToken(extraClaims, user);
     }
 
     public long getExpirationTime() {
         return jwtExpiration;
     }
 
-    private String doGenerateToken(Map<String, Object> claims, UserDetails userDetails) {
+    private String doGenerateToken(Map<String, Object> claims, User user) {
         return Jwts.builder()
                 .claims(claims)
-                .subject(userDetails.getUsername())
+                .subject(user.getUsername())
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + this.jwtExpiration))
                 .signWith(getSignInKey(), Jwts.SIG.HS256)
@@ -58,6 +59,22 @@ public class JwtService {
     public boolean isTokenValid(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    public JWTVerificationResponse verifyToken(String token) {
+        JWTVerificationResponse jwtVerificationResponse;
+        try {
+            Claims claims = extractAllClaims(token);
+
+            jwtVerificationResponse = new JWTVerificationResponse();
+            jwtVerificationResponse.setIsValid(true);
+            jwtVerificationResponse.setClaims(claims);
+
+
+            return jwtVerificationResponse;
+        } catch (Exception ignored) {
+            throw new JWTVerificationException("JWT token is not valid.");
+        }
     }
 
     private boolean isTokenExpired(String token) {
